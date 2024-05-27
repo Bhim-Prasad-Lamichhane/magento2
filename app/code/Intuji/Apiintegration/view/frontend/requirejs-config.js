3de9c25c02386e2b995044be4db5ab6443f880d5
Bhim-Prasad-Lamichhane <bhim.lamichhane@intuji.com>
2024-05-27 13:59:23 +0545

    googlemap integrated

---
+++ app/code/Intuji/Apiintegration/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            'googleMaps': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBPI4ia-BPEUmKHY0ZG6GL1XhFDyyjh03g&libraries=places&callback=initAutocomplete&v=weekly&loading=async'
+        }
+    }
+};
